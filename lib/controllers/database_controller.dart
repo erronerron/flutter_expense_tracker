2de5962cad7262eae8c82fe3f4bd9172197d4685
Erron Intila <88467532+erronerron@users.noreply.github.com>
2022-04-20 17:08:40 +0800

    Modify models and controllers

---
+++ lib/controllers/database_controller.dart
@@ -0,0 +1,42 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseController {
+  Future<Database> initializedDB() async {
+    String path = await getDatabasesPath();
+    return openDatabase(
+      // Set the path to the database. Note: Using the `join` function from the
+      // `path` package is best practice to ensure the path is correctly
+      // constructed for each platform.
+      join(path, 'expenses.db'),
+      // Set the version. This executes the onCreate function and provides a
+      // path to perform database upgrades and downgrades.
+      version: 1,
+      // When the database is first created, create a table to store records.
+      onCreate: (Database db, int version) async {
+        // Run the CREATE TABLE statements on the database.
+        await db.execute(
+          "CREATE TABLE users(id INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, email TEXT NOT NULL, currency TEXT, icon TEXT, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL)",
+        );
+        await db.execute(
+          "CREATE TABLE categories(id INTEGER PRIMARY KEY, name TEXT NOT NULL, icon TEXT, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL)",
+        );
+        await db.execute(
+          "CREATE TABLE expenses(id INTEGER PRIMARY KEY, description TEXT NOT NULL, amount INTEGER NOT NULL, paidAt TEXT NOT NULL, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL, categoryId INTEGER NOT NULL, FOREIGN KEY(categoryId) REFERENCES categories(id))",
+        );
+        await db.execute(
+          "CREATE TABLE incomes(id INTEGER PRIMARY KEY, description TEXT NOT NULL, amount INTEGER NOT NULL, startDate TEXT NOT NULL, endDate TEXT NOT NULL, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL)",
+        );
+        await db.execute(
+          "CREATE TABLE spending_limits(id INTEGER PRIMARY KEY, amount INTEGER NOT NULL, startDate TEXT NOT NULL, endDate TEXT NOT NULL, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL)",
+        );
+        await db.execute(
+          "CREATE TABLE transactions(id INTEGER PRIMARY KEY, transactionId INTEGER NOT NULL, transactionType TEXT NOT NULL, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL)",
+        );
+        await db.execute(
+          "INSERT INTO categories(name, icon, createdAt, updatedAt) VALUES ('Food', '', '${DateTime.now().toString()}', '${DateTime.now().toString()}'), ('Electricity', '', '${DateTime.now().toString()}', '${DateTime.now().toString()}'), ('Water', '', '${DateTime.now().toString()}', '${DateTime.now().toString()}')",
+        );
+      },
+    );
+  }
+}
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,23 +1,39 @@
 class Expense {
   late final int id;
+  late final int categoryId;
   late final String description;
   late final int amount;
+  late final String paidAt;
+  late final String createdAt;
+  late final String updatedAt;
 
   Expense({
     required this.id,
+    required this.categoryId,
     required this.description,
     required this.amount,
+    required this.paidAt,
+    required this.createdAt,
+    required this.updatedAt,
   });
 
   Expense.fromMap(Map<String, dynamic> result)
       : id = result["id"],
+        categoryId = result["categoryId"],
         description = result["description"],
-        amount = result["amount"];
+        amount = result["amount"],
+        paidAt = result["paidAt"],
+        createdAt = result["createdAt"],
+        updatedAt = result["updatedAt"];
   Map<String, Object> toMap() {
     return {
       'id': id,
+      'categoryId': categoryId,
       'description': description,
       'amount': amount,
+      'paidAt': paidAt,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
     };
   }
 }
