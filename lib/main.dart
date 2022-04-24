41f54aa191471b8a6af907d1099abb6357fd0919
Erron Intila <88467532+erronerron@users.noreply.github.com>
2022-04-24 20:34:24 +0800

    Add flutter orm for sqlite

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/models/model.dart';
 import 'package:flutter_expense_tracker/routes.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'provider/test_provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final bool isInitialized = await ExpenseDBModel().initializeDB();
+  if (isInitialized) runSamples();
+  print(isInitialized);
+
   runApp(ChangeNotifierProvider<TestProvider>(
     child: const MyApp(),
     create: (_) => TestProvider(), // Create a new ChangeNotifier object
@@ -26,4 +33,79 @@ class MyApp extends StatelessWidget {
       routes: appRoutes,
     );
   }
+}
+
+Future<bool> runSamples() async {
+  // add sample categories
+  await addSampleCategories();
+
+  // add sample expenses
+  await addSampleExpenses();
+
+  return true;
+}
+
+Future<void> addSampleCategories() async {
+  final category = await Category().select().toSingle();
+  if (category == null) {
+    await Category(
+      name: 'Food',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+    await Category(
+      name: 'Electricity',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+    await Category(
+      name: 'Water',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+    print('Sample categories successfully created');
+    return;
+  }
+  print(
+      'Error creating sample categories. There are categories already created.');
+  return;
+}
+
+Future<void> addSampleExpenses() async {
+  final expense = await Expense().select().toSingle();
+  final category = await Category().select().toSingle();
+  DateTime datetime =
+      DateTime.parse(DateFormat('yyyy-MM-dd').format(DateTime.now()));
+
+  if (expense == null && category != null) {
+    await Expense(
+      description: 'Lunch',
+      amount: 100,
+      paid_at: datetime,
+      categoryId: category.id,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+    await Expense(
+      description: 'Electricity Bill',
+      amount: 1000,
+      paid_at: datetime,
+      categoryId: category.id,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+    await Expense(
+      description: 'Water bill',
+      amount: 500,
+      paid_at: datetime,
+      categoryId: category.id,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+
+    print('Sample expenses successfully created');
+    return;
+  }
+  print('Error creating sample expenses. There are expenses already created.');
+  return;
 }
